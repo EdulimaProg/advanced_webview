0b119ac80ab91159774a409b8f3cab47ec18b730
EdulimaProg <42524674+EdulimaProg@users.noreply.github.com>
2024-08-27 11:03:18 -0300

    removendo getx

--- lib/homepage/home_page.dart
+++ lib/homepage/home_page.dart
@@ -1,7 +1,6 @@
-import 'package:advanced_webview/homepage/home_controller.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
-import 'package:get/get.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,19 +9,67 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  final HomeController _controller = Get.put(HomeController());
+  final GlobalKey webViewKey = GlobalKey();
+  PullToRefreshController? pullToRefreshController;
+  InAppWebViewController? webViewController;
+  String url = "https://www.firds.com.br/";
+  bool loading = false;
+
+  InAppWebViewSettings settings = InAppWebViewSettings(
+      isInspectable: kDebugMode,
+      mediaPlaybackRequiresUserGesture: false,
+      allowsInlineMediaPlayback: true,
+      iframeAllow: "camera; microphone",
+      iframeAllowFullscreen: true);
+
+  @override
+  void initState() {
+    super.initState();
+
+    pullToRefreshController = kIsWeb
+        ? null
+        : PullToRefreshController(
+            settings: PullToRefreshSettings(
+              color: Colors.blue,
+            ),
+            onRefresh: () async {
+              if (defaultTargetPlatform == TargetPlatform.android) {
+                webViewController?.reload();
+              } else if (defaultTargetPlatform == TargetPlatform.iOS) {
+                webViewController?.loadUrl(
+                    urlRequest:
+                        URLRequest(url: await webViewController?.getUrl()));
+              }
+            },
+          );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Obx(() => !_controller.loading.value ? const Center(child: CircularProgressIndicator()) : InAppWebView(
-          key: _controller.webViewKey,
-          initialUrlRequest: _controller.url,
-          initialSettings: _controller.settings,
-          pullToRefreshController: _controller.pullRefreshController,
-          onWebViewCreated: (_) => _controller.onWebViewCreated,
-          onPermissionRequest: _controller.requestPermission,
-        ),),
+        child: Stack(
+          children: [
+            InAppWebView(
+              key: webViewKey,
+              initialUrlRequest: URLRequest(url: WebUri(url)),
+              onLoadStart: (_, url) => setState(() {
+                loading = true;
+              }),
+              onLoadStop: (_, url) => setState(() {
+                loading = false;
+              }),
+              initialSettings: settings,
+              pullToRefreshController: pullToRefreshController,
+              onPermissionRequest: (controller, request) async {
+                return PermissionResponse(
+                    resources: request.resources,
+                    action: PermissionResponseAction.GRANT);
+              },
+            ),
+            loading == true ? const Center(child: CircularProgressIndicator(),): Container(),
+          ],
+        ),
       ),
     );
   }
